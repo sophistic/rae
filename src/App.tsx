2e884374f6243faa9ee40e0d24ce26bf3da7e15b
kushagra2503 <yashyyy831@gmail.com>
2025-07-29 14:48:16 +0530

    Testing shits

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,10 @@
-import { useState } from "react";
-import { invoke } from "@tauri-apps/api/core";
-import "./App.css";
+import React from 'react';
+import Onboarding from './components/views/OnBoardings';
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
-
-  async function greet() {
-    setGreetMsg(await invoke("greet", { name }));
-  }
-
   return (
-    <div>
-      <input
-        id="greet-input"
-        onChange={(e) => setName(e.currentTarget.value)}
-        placeholder="Enter a name..."
-      />
-      <button onClick={() => greet()}>Greet</button>
-
-      <p className="text-red-600">{greetMsg}</p>
+    <div className="App">
+      <Onboarding />
     </div>
   );
 }
