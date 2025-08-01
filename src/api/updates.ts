3d51950e2a7332a3715571200132fcf05c2e64b1
Nihal Rahman <rahmannihal72@gmail.com>
2025-08-02 02:03:21 +0530

    clear

---
+++ src/api/updates.ts
@@ -0,0 +1,9 @@
+import axios from "axios";
+import { useUserStore } from "@/store/userStore";
+
+const BASE_URL = "https://quackback-xwhd.onrender.com/api/auth";
+
+const { email } = useUserStore();
+export const NameUpdate = async (name: string) => {
+  //pass
+};
