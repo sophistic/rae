ed77502679a47eb738d6cf5008ab9e5dffea0cec
kushagra2503 <yashyyy831@gmail.com>
2025-08-05 18:46:58 +0530

    Removed the unused mic button

--- src/routes/magicDot/magicDot.tsx
+++ src/routes/magicDot/magicDot.tsx
@@ -152,26 +152,9 @@ const MagicDot = () => {
             </button>
             <button
               onClick={handleFollowClick}
-              className="no-drag hover:bg-gray-300 rounded p-2 border-r "
+              className="no-drag hover:bg-gray-300 rounded p-1 "
             >
               <Torus className="scale-75" />
-            </button>
-            <button className="no-drag hover:bg-gray-200 rounded p-2">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <path d="M12 2a3 3 0 0 0-3 3v7a3 3 0 0 0 6 0V5a3 3 0 0 0-3-3Z" />
-                <path d="M19 10v2a7 7 0 0 1-14 0v-2" />
-                <line x1="12" x2="12" y1="19" y2="22" />
-              </svg>
             </button>
           </div>
         </main>
